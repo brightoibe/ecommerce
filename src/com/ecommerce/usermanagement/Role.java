0eba3748cd2f1c3089735ae413ada0c78bd9c0fa
Bright Ibezim <brightoibe@yahoo.com>
2025-02-04 11:06:42 +0100

    Added fields to Role class

--- src/com/ecommerce/usermanagement/Role.java
+++ src/com/ecommerce/usermanagement/Role.java
@@ -10,5 +10,34 @@ package com.ecommerce.usermanagement;
  * @author The Bright
  */
 public class Role {
-    
+
+    /**
+     * @return the roleID
+     */
+    public String getRoleID() {
+        return roleID;
+    }
+
+    /**
+     * @param roleID the roleID to set
+     */
+    public void setRoleID(String roleID) {
+        this.roleID = roleID;
+    }
+
+    /**
+     * @return the roleName
+     */
+    public String getRoleName() {
+        return roleName;
+    }
+
+    /**
+     * @param roleName the roleName to set
+     */
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+    private String roleID;
+    private String roleName;
 }
