4b2e00e7f755769496f0885ce42b42eec9b02070
Bright Ibezim <brightoibe@yahoo.com>
2025-02-04 11:03:30 +0100

    Created the User class

---
+++ src/com/ecommerce/usermanagement/User.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ecommerce.usermanagement;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author The Bright
+ */
+public class User {
+    private String userID;
+    private String password_hash;
+    private String fullName;
+    private String gender;
+    private LocalDate dateOfBirth;
+    private String email;
+
+    /**
+     * @return the userID
+     */
+    public String getUserID() {
+        return userID;
+    }
+
+    /**
+     * @param userID the userID to set
+     */
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    /**
+     * @return the password_hash
+     */
+    public String getPassword_hash() {
+        return password_hash;
+    }
+
+    /**
+     * @param password_hash the password_hash to set
+     */
+    public void setPassword_hash(String password_hash) {
+        this.password_hash = password_hash;
+    }
+
+    /**
+     * @return the fullName
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * @param fullName the fullName to set
+     */
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the dateOfBirth
+     */
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    /**
+     * @param dateOfBirth the dateOfBirth to set
+     */
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
