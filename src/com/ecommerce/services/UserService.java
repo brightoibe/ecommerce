5793a41b932662ef0a788ab33d51f5f2fa0e8c25
Bright Ibezim <brightoibe@yahoo.com>
2025-02-04 12:05:17 +0100

    Created get and set methods to the userList static variables.

--- src/com/ecommerce/services/UserService.java
+++ src/com/ecommerce/services/UserService.java
@@ -14,14 +14,36 @@ import java.util.List;
  * @author The Bright
  */
 public class UserService {
+
+    /**
+     * @return the registeredUserList
+     */
+    public static List<User> getRegisteredUserList() {
+        return registeredUserList;
+    }
+
+    /**
+     * @param aRegisteredUserList the registeredUserList to set
+     */
+    public static void setRegisteredUserList(List<User> aRegisteredUserList) {
+        registeredUserList = aRegisteredUserList;
+    }
     
     private static List<User> registeredUserList;
     
     private static UserService userServiceInstance;
     
+    /*Made the constructor private so a UserService cannot be instanciated from outside
+    the class.
+    */
+    
     private UserService(){
         registeredUserList=new ArrayList<User>();
     }
+    /*
+       This method ensures that only one instance of this class can exist
+       at a time
+    */
     public static UserService getInstance() {
         if (userServiceInstance == null) {
             userServiceInstance = new UserService();
